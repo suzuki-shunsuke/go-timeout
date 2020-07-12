b0dfe876aa8783875ff41918e8c93fb21ca49c34
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-07-12 20:07:25 +0900

    refactor: change pointer to struct

--- timeout/error.go
+++ timeout/error.go
@@ -20,34 +20,34 @@ type (
 	}
 )
 
-func (se *StartError) Error() string {
+func (se StartError) Error() string {
 	return se.err.Error()
 }
 
-func (se *StartError) Cause() error {
+func (se StartError) Cause() error {
 	return se.err
 }
 
-func (we *WaitError) Error() string {
+func (we WaitError) Error() string {
 	return we.err.Error()
 }
 
-func (we *WaitError) Cause() error {
+func (we WaitError) Cause() error {
 	return we.err
 }
 
-func (ke *KillError) Error() string {
+func (ke KillError) Error() string {
 	return ke.err.Error()
 }
 
-func (ke *KillError) Cause() error {
+func (ke KillError) Cause() error {
 	return ke.err
 }
 
-func (ke *KillError) Signal() syscall.Signal {
+func (ke KillError) Signal() syscall.Signal {
 	return ke.sig
 }
 
-func (ke *KillError) PID() int {
+func (ke KillError) PID() int {
 	return ke.id
 }

257300fb4ab6298be6fdcc2a6ee51bfbd55ca180
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-11-28 11:51:59 +0900

    chore: rename aqua org

--- ci/test.sh
+++ ci/test.sh
@@ -5,6 +5,6 @@ set -eu
 cd "$(dirname "$0")/.."
 
 export PATH=$HOME/.aqua/bin:$PATH
-curl -sSfL https://raw.githubusercontent.com/suzuki-shunsuke/aqua-installer/v0.2.0/aqua-installer | bash
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v0.3.0/aqua-installer | bash
 aqua i -l
 bash scripts/test-code-climate.sh
